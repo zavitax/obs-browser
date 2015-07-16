252bde78160397e40278fd9813d566ea4e354fd2
jp9000 <obs.jim@gmail.com>
2015-07-16 16:10:16 -0700

    Improve FindCEF.cmake useage on non-mac
    This allows you to use an option CEFPath environment variable or cmake
    variable to specify the path to the base CEF source directory (in
    addition to CEF_ROOT_PATH.
    
    Also allows the ability to specify the CEF build type ("Release" or
    "Debug") via the CEFBuildType cmake variable (which defaults to
    "Release" if not set).

--- FindCEF.cmake
+++ FindCEF.cmake
@@ -1,17 +1,57 @@
 include(FindPackageHandleStandardArgs)
 
-message("Looking for Chromium Embedded Framework in ${CEF_ROOT_DIR}")
+find_package(PkgConfig QUIET)
+if (PKG_CONFIG_FOUND)
+	pkg_check_modules(_OBS QUIET cef)
+endif()
+
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	set(_lib_suffix 64)
+else()
+	set(_lib_suffix 32)
+endif()
+
+if(NOT DEFINED CEFBuildType)
+	set(CEFBuildType "Release")
+endif()
 
 find_path(CEF_INCLUDE_DIR "include/cef_version.h"
-	HINTS ${CEF_ROOT_DIR})
+	HINTS
+		ENV CEFPath
+		ENV CEFPath${_lib_suffix}
+		${CEF_ROOT_DIR}
+		${CEFPath}
+	PATHS
+		/usr/include /usr/local/include /opt/local/include /sw/include
+	)
 
 find_library(CEF_LIBRARY
 	NAMES cef libcef cef.lib libcef.o "Chromium Embedded Framework"
-	PATHS ${CEF_ROOT_DIR} ${CEF_ROOT_DIR}/Release)
+	PATHS
+		/usr/include /usr/local/include /opt/local/include /sw/include
+		ENV CEFPath
+		ENV CEFPath${_lib_suffix}
+		${CEF_ROOT_DIR}
+		${CEFPath}
+	PATH_SUFFIXES
+		${CEFBuildType}
+	)
 
 find_library(CEFWRAPPER_LIBRARY
 	NAMES cef_dll_wrapper libcef_dll_wrapper
-	PATHS ${CEF_ROOT_DIR}/build/libcef_dll)
+	PATHS
+		${CEF_ROOT_DIR}
+		${CEF_INCLUDE_DIR}
+	PATH_SUFFIXES
+		build/libcef_dll/${CEFBuildType}
+		build/libcef_dll
+		build${_lib_suffix}/libcef_dll/${CEFBuildType}
+		build${_lib_suffix}/libcef_dll
+		../build/libcef_dll/${CEFBuildType}
+		../build/libcef_dll
+		../build${_lib_suffix}/libcef_dll/${CEFBuildType}
+		../build${_lib_suffix}/libcef_dll
+	)
 
 if (NOT CEF_LIBRARY)
 	message(FATAL_ERROR "Could not find the CEF shared library" )
@@ -30,3 +70,7 @@ find_package_handle_standard_args(CEF DEFAULT_MSG CEF_LIBRARY
 
 mark_as_advanced(CEF_LIBRARY CEF_WRAPPER_LIBRARY CEF_LIBRARIES 
 	CEF_INCLUDE_DIR)
+
+if(NOT DEFINED CEF_ROOT_DIR)
+	set(CEF_ROOT_DIR "${CEF_INCLUDE_DIR}")
+endif()
